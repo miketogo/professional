52665232fd255e1385d2d5f877c906a9c9974bcc
Mikhail Surikov <miketogo@yandex.ru>
2020-11-15 05:16:20 +0300

    почти доделал вылезающее меню, осталось затемнение фона

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $('.menu__burger').on('click', function(e){
   e.preventDefault;
   $('.swipe-menu').toggleClass('swipe-menu_active');
+  $('.background-fade').toggleClass('background-fade_active');
+  $('body').addClass('stop-scrolling');
 });
 
 $('.swipe-menu__exit_touch-zone').on('click', function(e){
   e.preventDefault;
   $('.swipe-menu').removeClass('swipe-menu_active');
+  $('body').removeClass('stop-scrolling');
 });
