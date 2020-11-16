3ae64e91b6ddbd9d841e5b13d96e6d23711c66e0
Mikhail Surikov <miketogo@yandex.ru>
2020-11-16 21:51:28 +0300

    test swipe.js 3 fix backgraund-darker opacity bag

--- swipe.js
+++ swipe.js
@@ -9,18 +9,25 @@ container.addEventListener('swipe', function (e) {
 
   if (screen.width < 1351){
   if (directions.right) {
-    console.log('Swiped right.');
+    // console.log('Swiped right.');
       $('.swipe-menu').toggleClass('swipe-menu_active');
       $('.background-darker').toggleClass('background-darker_active');
       $('body').addClass('stop-scrolling');
   }
   if (directions.left) {
-    console.log('Swiped left.');
+    // console.log('Swiped left.');
       $('.swipe-menu').removeClass('swipe-menu_active');
       $('body').removeClass('stop-scrolling');
-      $('.background-darker').removeClass('background-darker_active');
+      $('.background-darker').toggleClass('background-darker_deactive');
+      setTimeout(
+        () => {
+          $('.background-darker').removeClass('background-darker_active');
+          $('.background-darker').removeClass('background-darker_deactive');
+        },
+        0.6 * 1000
+      );
   }};
 
-  console.log('Started horizontally at', x[0], 'and ended at', x[1]);
-  console.log('Started vertically at', y[0], 'and ended at', y[1]);
+  // console.log('Started horizontally at', x[0], 'and ended at', x[1]);
+  // console.log('Started vertically at', y[0], 'and ended at', y[1]);
 });
