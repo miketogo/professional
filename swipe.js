933b1a50a761c0a8a9704ec5e594da3bad01c8d0
Mikhail Surikov <miketogo@yandex.ru>
2020-11-16 22:30:09 +0300

    swap menu lunks fixed with margin, выключил подсветку при нажатии в руте

--- swipe.js
+++ swipe.js
@@ -8,26 +8,29 @@ container.addEventListener('swipe', function (e) {
   var y = e.detail.y;
 
   if (screen.width < 1351){
-  if (directions.right) {
-    // console.log('Swiped right.');
-      $('.swipe-menu').toggleClass('swipe-menu_active');
-      $('.background-darker').toggleClass('background-darker_active');
-      $('body').addClass('stop-scrolling');
+  if (Math.abs(x[0]-x[1])>150){
+    if (directions.right) {
+      console.log('Swiped right.');
+        $('.swipe-menu').toggleClass('swipe-menu_active');
+        $('.background-darker').toggleClass('background-darker_active');
+        $('body').addClass('stop-scrolling');
+    }
+    if (directions.left) {
+      console.log('Swiped left.');
+        $('.swipe-menu').removeClass('swipe-menu_active');
+        $('body').removeClass('stop-scrolling');
+        $('.background-darker').toggleClass('background-darker_deactive');
+        setTimeout(
+          () => {
+            $('.background-darker').removeClass('background-darker_active');
+            $('.background-darker').removeClass('background-darker_deactive');
+          },
+          0.6 * 1000
+        );
+    }};
   }
-  if (directions.left) {
-    // console.log('Swiped left.');
-      $('.swipe-menu').removeClass('swipe-menu_active');
-      $('body').removeClass('stop-scrolling');
-      $('.background-darker').toggleClass('background-darker_deactive');
-      setTimeout(
-        () => {
-          $('.background-darker').removeClass('background-darker_active');
-          $('.background-darker').removeClass('background-darker_deactive');
-        },
-        0.6 * 1000
-      );
-  }};
 
-  // console.log('Started horizontally at', x[0], 'and ended at', x[1]);
-  // console.log('Started vertically at', y[0], 'and ended at', y[1]);
+
+  console.log('Started horizontally at', x[0], 'and ended at', x[1]);
+  console.log('Started vertically at', y[0], 'and ended at', y[1]);
 });
