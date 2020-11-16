893955fcd867a8e22e80955c222decdb2591f242
Mikhail Surikov <miketogo@yandex.ru>
2020-11-16 21:30:17 +0300

    test swipe.js 2 + fix banner-button

--- swipe.js
+++ swipe.js
@@ -7,7 +7,7 @@ container.addEventListener('swipe', function (e) {
   var x = e.detail.x;
   var y = e.detail.y;
 
-
+  if (screen.width < 1351){
   if (directions.right) {
     console.log('Swiped right.');
       $('.swipe-menu').toggleClass('swipe-menu_active');
@@ -19,7 +19,7 @@ container.addEventListener('swipe', function (e) {
       $('.swipe-menu').removeClass('swipe-menu_active');
       $('body').removeClass('stop-scrolling');
       $('.background-darker').removeClass('background-darker_active');
-  }
+  }};
 
   console.log('Started horizontally at', x[0], 'and ended at', x[1]);
   console.log('Started vertically at', y[0], 'and ended at', y[1]);
